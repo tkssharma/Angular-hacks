adc23282b2d0cf90e858378890bb97fda2e667fb
Tarun Sharma <tarun.softengg@gmail.com>
2017-04-02 07:13:35 +0530

    updates

--- $scope.$emit/communication.js
+++ $scope.$emit/communication.js
@@ -11,11 +11,15 @@ $scope.$on('myCustomEvent', function (event, data) {
   console.log(data); // 'Data to send'
 });
 <!--  -->
+
+
+
 <div ng-controller="ParentCtrl as parent" class="ng-scope">
   {{ parent.data }}
-  <div ng-controller="SiblingOneCtrl as sib1" class="ng-scope">
-      {{ sib1.data }}
-  </div>
+      <div ng-controller="SiblingOneCtrl as sib1" class="ng-scope">
+          {{ sib1.data }}
+             {{ parent.data }}
+      </div>
 </div>
 
 app.controller('ParentCtrl',
--- ng-test/example/src/app/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var app = angular.module('webApp', ['myControllerModule', 'myDirectiveModule', 'myProviderModule', 'myComponentModule']);
-
-app.config(function (helloWorldProvider) {
-  helloWorldProvider.configure('Pablo');
-});
